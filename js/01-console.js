94d19e70737b85d7783bda701e26d75d41e34d16
MonikaJaraite <monjara@gmail.com>
2023-01-16 23:41:42 +0200

    examples

--- js/01-console.js
+++ js/01-console.js
@@ -12,3 +12,12 @@ console.log(5);
 console.log(3, 14);
 console.log(3.14);
 console.log(1, 2, 3, 4, 5, 6);
+
+/*
+Esmines busimos temos:
+- kintamieji (atsiminti informacija, duomenys)
+- ciklai (proceduru kartojimas)
+- funkcijos (pvz, vidurkio skaiciavimas logikoje)
+[pirmos trys temos terminale]
+- event listener [narsykleje]
+*/
