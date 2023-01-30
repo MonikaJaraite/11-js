269b5ed87be8b535f9fb6943521a67e21ff3c668
MonikaJaraite <monjara@gmail.com>
2023-01-30 22:14:31 +0200

    Codewars

--- js/33-codewars.js
+++ js/33-codewars.js
@@ -136,3 +136,20 @@ function removeChar(str){
 */
 console.log('--------------');
 
+// CODEWARS:
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons){
+  if (bullets >= dragons * 2) {
+   return true }
+   else {
+    return false;
+   }
+}
+
+  console.log(hero(10, 5), true);
+  console.log(hero(7, 4), false);
+  console.log(hero(4, 5), false);
+  console.log(hero(100, 40), true);
+  console.log(hero(1500, 751), false);
+  console.log(hero(0, 1), false);
